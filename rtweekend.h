db83030a05842f10e99cf9345d059deff321305f
Daniel Pfeifer <pfeifer.daniel@gmx.at>
2023-03-06 20:47:21 +0100

    added list class for hittable objects, general header and adapted main

---
+++ rtweekend.h
@@ -0,0 +1,30 @@
+//
+// Created by Daniel Pfeifer on 3/6/23.
+//
+
+#ifndef RAYTRACING_RTWEEKEND_H
+#define RAYTRACING_RTWEEKEND_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+// Using
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+// Constants
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+// Utility functions
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+// Common headers
+#include "ray.h"
+#include "vec3.h"
+
+#endif //RAYTRACING_RTWEEKEND_H
