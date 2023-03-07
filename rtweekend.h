f7ffcd55b53159207dfafcc1a046d6cc6880d879
Daniel Pfeifer <pfeifer.daniel@gmx.at>
2023-03-07 11:58:40 +0100

    added camera class and antialiasing

--- rtweekend.h
+++ rtweekend.h
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // Using
 using std::shared_ptr;
@@ -26,5 +27,19 @@ inline double degrees_to_radians(double degrees) {
 // Common headers
 #include "ray.h"
 #include "vec3.h"
+
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min+ (max-min) * random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if(x < min) return min;
+    if(x > max) return max;
+    return x;
+}
 
 #endif //RAYTRACING_RTWEEKEND_H
